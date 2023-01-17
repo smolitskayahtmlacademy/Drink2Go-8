db7a213ab12e96c08b4a69d88901e6c4836c4101
iugoia <khabirovvaelvina12@gmail.com>
2023-01-17 21:05:40 +0300

    first commit

---
+++ source/js/script.js
@@ -0,0 +1,64 @@
+/* в этот файл добавляет скрипты*/
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+window.onload = function () {
+  slideOne();
+  slideTwo();
+}
+let sliderOne = document.getElementById("slider__1");
+let sliderTwo = document.getElementById("slider__2");
+let displayValOne = document.getElementById("range1");
+let displayValTwo = document.getElementById("range2");
+let minGap = 0;
+let sliderTrack = document.querySelector(".slider__track");
+let sliderMaxValue = document.getElementById("slider__1").max;
+function slideOne() {
+  if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
+    sliderOne.value = parseInt(sliderTwo.value) - minGap;
+  }
+  displayValOne.value = sliderOne.value;
+  fillColor();
+}
+function slideTwo() {
+  if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
+    sliderTwo.value = parseInt(sliderOne.value) + minGap;
+  }
+  displayValTwo.value = sliderTwo.value;
+  fillColor();
+}
+function fillColor() {
+  percent1 = (sliderOne.value / sliderMaxValue) * 100;
+  percent2 = (sliderTwo.value / sliderMaxValue) * 100;
+  sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #9070EC ${percent1}% , #9070EC ${percent2}%, #dadae5 ${percent2}%)`;
+}
+
+const selectSingle = document.querySelector(".__select");
+const selectSingle_title = selectSingle.querySelector(".__select__title");
+const selectSingle_labels = selectSingle.querySelectorAll(".__select__label");
+
+selectSingle_title.addEventListener("click", () => {
+  if ("active" === selectSingle.getAttribute("data-state")) {
+    selectSingle.setAttribute("data-state", "");
+  } else {
+    selectSingle.setAttribute("data-state", "active");
+  }
+});
+
+for (let i = 0; i < selectSingle_labels.length; i++) {
+  selectSingle_labels[i].addEventListener("click", (evt) => {
+    selectSingle_title.textContent = evt.target.textContent;
+    selectSingle.setAttribute("data-state", "");
+  });
+}
